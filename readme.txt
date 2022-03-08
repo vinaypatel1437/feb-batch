6fcddd383618bc52ace4698f0c01147085c21587
vinaypatel1437 <patel.vp.vinay@gmail.com>
2022-03-08 22:35:25 +0530

    First Commit

---
+++ readme.txt
@@ -0,0 +1,18 @@
+
+    1. querySelector :- Wo element utha ke lata hai jo usko pehle milta hai
+    2. querySelectorAll :- Sare element uthata hai or ek array ke form me lake dedeta hai.
+    3. getElementById :- Id match krta hai or uske corresponding jo bhi cheez lake dedega.
+    4. addEventListener :- Hum jo bhi event lagate hai usko listen krta hai.
+    5. innerHTML :- Tag ke andar se hame HTML ka code lake dedega.
+    6. innerText :- Tag ke andar se hame Text lake dedega.
+    7. getElementByClassName :- Class match karke querySelectorAll jese array lake dedega.
+    8. value :- input tag ki value ko lake dedega .
+    9. event :- event jo hai wo sirf or sirf form tag me work krta hai.
+    10. Events:- click,onmouseenter,mouseleave.
+    11. let,var,const,kuch bhi nahi.
+
+
+
+    Kal ka task :- 
+    1. e padh kr aana hai.
+    2. UI design krna hai.
---
+++ script.js
@@ -0,0 +1,67 @@
+// let clg = document.getElementById("clg_ki_id");
+// console.log(clg1);
+// let meri_class = document.querySelector(".ek_or_class");
+// let new_class = document.querySelector(".new_class");
+// console.log(new_class.innerHTML); // innerHTML:- Hamare div ke andar se HTML ka code lake dedeta hai
+// console.log(new_class.innerText); //innerText :- Hamare div ke andar se Text lake dedega
+
+// let, var, const, kuch bhi nahi 
+// var :- redeclare, reassign
+// let :- redeclare nahi kr skte , reassign kar sakte hai
+// const:- redeclare nhi kr skte, reassign bhi nahi kr skte
+// kuch bhi nahi :- var ki tarah work krta hai
+
+// a = 10;
+// console.log(a);
+// a = 20;
+// console.log(a);
+
+// let input_tag = document.querySelector(".input_tag");
+// let btn = document.querySelector(".btn");
+
+// btn.addEventListener("click",function (){
+//     console.log(input_tag.value);
+//     input_tag.value = "";
+// })
+
+
+// let input_tag = document.querySelector(".input_tag");
+// let btn = document.querySelector(".btn");
+
+
+
+// function koi_sa_function(e) {
+//     console.log(e.current.value)
+//     console.log(input_tag.value)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
